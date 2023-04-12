2c1c92230f917b79c0a128b5f5e9d6fed331b81d
Alexie81 <alexie.popescu2019@yahoo.com>
2023-04-12 12:22:14 +0300

    create login system /admin and update todo

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,27 +1,137 @@
-import logo from './../logo.svg';
-import './css/HeaderSearch.css';
 import * as React from 'react';
-import {useState} from 'react';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import seosky from './../images/linkedin_banner_image_1.png';
-import seosky_logo from './../images/youtube_profile_image.png';
-import { ProSidebarProvider, Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from 'react-pro-sidebar';
-import TextField from "@mui/material/TextField";
-import Tilty from 'react-tilty';
-import Categories from './../dbs/categories.json';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
-import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-import sslogo from './../images/sslogo.jpg';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Swal from 'sweetalert2';
+import adminData from "./../dbs/admin.json";
 
-
-function Admin() {
-return (
-  <div>
-    Hello from admin!
-    </div>
-)
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://seosky.ro">
+        SeoSky
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Admin;
+const theme = createTheme();
+
+export default function Admin() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    if(data.get('email') == "" && data.get('password') == "") {
+      Swal.fire({
+        icon: 'error',
+        title: 'Eroare!',
+        text: 'Va rugam introduceti adresa de email si parola!',
+        footer: 'Copyright © <a href="https://seosky.ro">SeoSky</a>'
+      });
+    } else if(data.get('email') == ""){
+      Swal.fire({
+        icon: 'error',
+        title: 'Eroare!',
+        text: 'Va rugam introduceti adresa de email!',
+        footer: 'Copyright © <a href="https://seosky.ro">SeoSky</a>'
+      });
+    } else if(data.get('password') == ""){
+      Swal.fire({
+        icon: 'error',
+        title: 'Eroare!',
+        text: 'Va rugam introduceti parola!',
+        footer: 'Copyright © <a href="https://seosky.ro">SeoSky</a>'
+      });
+    } else {
+      adminData.map(account => {
+        if(data.get("email") == account.email && data.get("password") == account.password){
+          Swal.fire({
+            icon: 'success',
+            title: 'Succes!',
+            text: 'Autentificare...',
+            footer: 'Copyright © <a href="https://seosky.ro">SeoSky</a>'
+          });
+        }
+      })
+      // if(data.get("email") == adminData[0].email){
+      //   Swal.fire({
+      //     icon: 'error',
+      //     title: 'Error!',
+      //     text: 'Please enter password!',
+      //     footer: 'Copyright © <a href="https://seosky.ro">SeoSky</a>'
+      //   });
+      // }
+    }
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
