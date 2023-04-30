859c5265c95f1970a4114f65b3d249f2e3b6fa52
Alexie81 <alexie.popescu2019@yahoo.com>
2023-04-30 20:23:08 +0300

    finish access app stats

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { deflateRawSync } = require('zlib');
+const {
+  deflateRawSync
+} = require('zlib');
 
 
 const app = express();
@@ -15,8 +17,45 @@ let products_stats = [];
 let admin_token = [];
 let admin_data = [];
 let admin_log = [];
+let access_stats = [];
 const monthNames = ["Ianuarie", "Februarie", "Martie", "Aprilie", "Mai", "Junie", "Julie", "August", "Septembrie", "Octombrie", "Noiembrie", "Decembrie"];
-const standardYearMonths = [{month:"Ianuarie", clicks:0}, {month:"Februarie", clicks:0}, {month:"Martie", clicks:0}, {month:"Aprilie", clicks:0}, {month:"Mai", clicks:0}, {month:"Iunie", clicks:0}, {month:"Iulie", clicks:0}, {month:"August", clicks:0}, {month:"Septembrie", clicks:0}, {month:"Octombrie", clicks:0}, {month:"Noiembrie", clicks:0}, {month:"Decembrie", clicks:0}]
+const standardYearMonths = [{
+  month: "Ianuarie",
+  clicks: 0
+}, {
+  month: "Februarie",
+  clicks: 0
+}, {
+  month: "Martie",
+  clicks: 0
+}, {
+  month: "Aprilie",
+  clicks: 0
+}, {
+  month: "Mai",
+  clicks: 0
+}, {
+  month: "Iunie",
+  clicks: 0
+}, {
+  month: "Iulie",
+  clicks: 0
+}, {
+  month: "August",
+  clicks: 0
+}, {
+  month: "Septembrie",
+  clicks: 0
+}, {
+  month: "Octombrie",
+  clicks: 0
+}, {
+  month: "Noiembrie",
+  clicks: 0
+}, {
+  month: "Decembrie",
+  clicks: 0
+}]
 
 
 
@@ -38,7 +77,9 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname, '/../client/public')));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 //
 //
@@ -73,7 +114,7 @@ app.post('/editCategory', (req, res) => {
   let category = req.body
   console.log(category);
   data2.map((item) => {
-    if(item.id == category.id){
+    if (item.id == category.id) {
       item.name = category.name;
       item.subcategories = category.subcategories;
     }
@@ -94,12 +135,12 @@ app.post('/resyncCategories', (req, res) => {
 });
 
 app.get("/getAllCategories", (req, res) => {
-  if(categories.length == 0){
+  if (categories.length == 0) {
     try {
       const data = fs.readFileSync(path.join(__dirname, '/dbs/categories.json'), 'utf8');
       console.log("Uploading backup DB");
       categories = JSON.parse(data);
-      
+
       res.send(JSON.stringify(categories, getCircularReplacer()));
     } catch (err) {
       console.error(err);
@@ -116,11 +157,15 @@ app.get("/deleteCategory", (req, res) => {
   const data = fs.readFileSync(path.join(__dirname, '/dbs/categories.json'), 'utf8');
   console.log("reading DB");
   let data2 = JSON.parse(data);
-  data2.splice((parseInt(id)-1), 1)
+  data2.splice((parseInt(id) - 1), 1)
   let i = 1;
   let data3 = [];
   data2.map((item) => {
-    data3.push({"id": i, "name": item.name, subcategories: item.subcategories});
+    data3.push({
+      "id": i,
+      "name": item.name,
+      subcategories: item.subcategories
+    });
     i++;
   });
   console.log(data3);
@@ -137,7 +182,7 @@ app.get("/deleteSubCategory", (req, res) => {
   const data = fs.readFileSync(path.join(__dirname, '/dbs/categories.json'), 'utf8');
   console.log("reading DB");
   let data2 = JSON.parse(data);
-  data2[parseInt(id)-1].subcategories.splice((parseInt(subid)-1), 1)
+  data2[parseInt(id) - 1].subcategories.splice((parseInt(subid) - 1), 1)
   console.log(data2);
   categories = data2;
   fs.writeFileSync(path.join(__dirname, '/dbs/categories.json'), JSON.stringify(data2, getCircularReplacer()));
@@ -150,7 +195,7 @@ app.get('/addSubCategory', (req, res) => {
   const data = fs.readFileSync(path.join(__dirname, '/dbs/categories.json'), 'utf8');
   console.log("reading DB");
   let data2 = JSON.parse(data);
-  data2[parseInt(id)-1].subcategories.push(name);
+  data2[parseInt(id) - 1].subcategories.push(name);
   fs.writeFileSync(path.join(__dirname, '/dbs/categories.json'), JSON.stringify(data2, getCircularReplacer()));
   res.send('record is added to the database');
 });
@@ -162,7 +207,7 @@ app.get('/editSubCategory', (req, res) => {
   const data = fs.readFileSync(path.join(__dirname, '/dbs/categories.json'), 'utf8');
   console.log("reading DB");
   let data2 = JSON.parse(data);
-  data2[parseInt(id)-1].subcategories[parseInt(subid)-1] = name;
+  data2[parseInt(id) - 1].subcategories[parseInt(subid) - 1] = name;
   fs.writeFileSync(path.join(__dirname, '/dbs/categories.json'), JSON.stringify(data2, getCircularReplacer()));
   res.send('record is added to the database');
 });
@@ -179,12 +224,12 @@ app.get('/editSubCategory', (req, res) => {
 //
 // ------------------------------------------------------------------------------------------PRODUCTS (API)-----------------------------------------------------------------------------
 app.get("/getAllProducts", (req, res) => {
-  if(products.length == 0){
+  if (products.length == 0) {
     try {
       const data = fs.readFileSync(path.join(__dirname, '/dbs/products.json'), 'utf8');
       console.log("Uploading backup DB");
       products = JSON.parse(data);
-      
+
       res.send(JSON.stringify(products, getCircularReplacer()));
     } catch (err) {
       console.error(err);
@@ -215,24 +260,24 @@ app.get("/deleteProduct", (req, res) => {
   const data = fs.readFileSync(path.join(__dirname, '/dbs/products.json'), 'utf8');
   console.log("reading DB");
   let data2 = JSON.parse(data);
-  data2.splice((parseInt(id)-1), 1)
+  data2.splice((parseInt(id) - 1), 1)
   let i = 1;
   let data3 = [];
   data2.map((item) => {
     // Add all data available in products.json
     data3.push({
       "id": i,
-          "date": item.date,
-          "clicks": item.clicks,
-          "name": item.name,
-          "subname": item.subname,
-          "price":  item.price,
-          "image": item.image,
-          "description": item.description,
-          "distribuitor": item.distribuitor,
-          "category": item.category,
-          "subcategory": item.subcategory,
-          "link": item.link
+      "date": item.date,
+      "clicks": item.clicks,
+      "name": item.name,
+      "subname": item.subname,
+      "price": item.price,
+      "image": item.image,
+      "description": item.description,
+      "distribuitor": item.distribuitor,
+      "category": item.category,
+      "subcategory": item.subcategory,
+      "link": item.link
     });
     i++;
   });
@@ -252,18 +297,18 @@ app.post('/editProduct', (req, res) => {
   let product = req.body
   console.log(product);
   data2.map((item) => {
-    if(item.id == product.id){
-        item.date = product.date;
-        item.clicks = product.clicks;
-        item.name = product.name;
-        item.subname = product.subname;
-        item.price = product.price;
-        item.image = product.image;
-        item.description = product.description;
-        item.distribuitor = product.distribuitor;
-        item.category = product.category;
-        item.subcategory = product.subcategory;
-        item.link = product.link;
+    if (item.id == product.id) {
+      item.date = product.date;
+      item.clicks = product.clicks;
+      item.name = product.name;
+      item.subname = product.subname;
+      item.price = product.price;
+      item.image = product.image;
+      item.description = product.description;
+      item.distribuitor = product.distribuitor;
+      item.category = product.category;
+      item.subcategory = product.subcategory;
+      item.link = product.link;
     }
   })
   products = data2;
@@ -283,12 +328,12 @@ app.post('/editProduct', (req, res) => {
 //
 // ------------------------------------------------------------------------------------------ADMIN-LOGIN (API)-----------------------------------------------------------------------------
 app.get("/getAdminData", (req, res) => {
-  if(admin_data.length == 0){
+  if (admin_data.length == 0) {
     try {
       const data = fs.readFileSync(path.join(__dirname, '/dbs/admin_data.json'), 'utf8');
       console.log("Uploading backup DB");
       admin_data = JSON.parse(data);
-      
+
       res.send(JSON.stringify(admin_data, getCircularReplacer()));
     } catch (err) {
       console.error(err);
@@ -305,11 +350,15 @@ app.get("/deleteAdminUser", (req, res) => {
   const data = fs.readFileSync(path.join(__dirname, '/dbs/admin_data.json'), 'utf8');
   console.log("reading DB");
   let data2 = JSON.parse(data);
-  data2.splice((parseInt(id)-1), 1)
+  data2.splice((parseInt(id) - 1), 1)
   let i = 1;
   let data3 = [];
   data2.map((item) => {
-    data3.push({"id": i, "email": item.email, "password": item.password});
+    data3.push({
+      "id": i,
+      "email": item.email,
+      "password": item.password
+    });
     i++;
   });
   console.log(data3);
@@ -327,7 +376,7 @@ app.post('/editAdminUser', (req, res) => {
   let category = req.body
   console.log(category);
   data2.map((item) => {
-    if(item.id == category.id){
+    if (item.id == category.id) {
       item.email = category.email;
       item.password = category.password;
     }
@@ -364,9 +413,11 @@ app.post('/addAdminUser', (req, res) => {
 app.get("/edit_A_D_M_I_NToken", (req, res) => {
   let name = req.query.name;
   let token = req.query.token;
-  if(name == "Alexie"){
+  if (name == "Alexie") {
     let data2;
-    data2 = [{token: token}];
+    data2 = [{
+      token: token
+    }];
     admin_token = data2;
     fs.writeFileSync(path.join(__dirname, '/dbs/admin_token.json'), JSON.stringify(data2, getCircularReplacer()));
     res.send('New token uploaded');
@@ -375,13 +426,13 @@ app.get("/edit_A_D_M_I_NToken", (req, res) => {
 
 app.get("/get_A_D_M_I_NToken", (req, res) => {
   let name = req.query.name;
-  if(name == "Alexie"){
-    if(admin_token.length == 0){
+  if (name == "Alexie") {
+    if (admin_token.length == 0) {
       try {
         const data = fs.readFileSync(path.join(__dirname, '/dbs/admin_token.json'), 'utf8');
         console.log("Uploading backup DB");
         admin_token = JSON.parse(data);
-        
+
         res.send(JSON.stringify(admin_token, getCircularReplacer()));
       } catch (err) {
         console.error(err);
@@ -407,12 +458,12 @@ app.post('/addAdminLog', (req, res) => {
 });
 
 app.get("/getAdminLog", (req, res) => {
-  if(admin_log.length == 0){
+  if (admin_log.length == 0) {
     try {
       const data = fs.readFileSync(path.join(__dirname, '/dbs/admin_log.json'), 'utf8');
       console.log("Uploading backup DB");
       admin_log = JSON.parse(data);
-      
+
       res.send(JSON.stringify(admin_log, getCircularReplacer()));
     } catch (err) {
       console.error(err);
@@ -445,51 +496,98 @@ app.get("/clickProduct", (req, res) => {
   const d = new Date();
   let currentYear = d.getFullYear();
   console.log("reading DB");
-  
+
   let data2 = JSON.parse(data);
   data2.map((item) => {
-    if(Number(item.id) === Number(product_id)) {
+    if (Number(item.id) === Number(product_id)) {
       item.clicks = Number(item.clicks) + 1
     }
   });
   products = data2;
   fs.writeFileSync(path.join(__dirname, '/dbs/products.json'), JSON.stringify(data2, getCircularReplacer()));
-    const products_clicks = fs.readFileSync(path.join(__dirname, '/dbs/products_clicks.json'), 'utf8');
-    let clicksProd = JSON.parse(products_clicks);
-    let v = [];
-    
-    if(clicksProd.length > 0){
-      let ises = false;
-      clicksProd.map((an) => {
-        if(an.an === currentYear){
-          ises = true;
-        }
+  const products_clicks = fs.readFileSync(path.join(__dirname, '/dbs/products_clicks.json'), 'utf8');
+  let clicksProd = JSON.parse(products_clicks);
+  let v = [];
+
+  if (clicksProd.length > 0) {
+    let ises = false;
+    clicksProd.map((an) => {
+      if (an.an === currentYear) {
+        ises = true;
+      }
+    });
+    if (!ises) {
+      clicksProd.push({
+        an: currentYear,
+        months: standardYearMonths
       });
-      if(!ises) {
-        clicksProd.push({an: currentYear, months: standardYearMonths});
-      }
-    } else {
-      clicksProd.push({an: currentYear, months: standardYearMonths});
     }
-      clicksProd.map((an) => {
-        
-        if(Number(an.an) === Number(currentYear)){
-          an.months.map((month) => {
-            if(month.month === monthNames[d.getMonth()]){
-              month.clicks = Number(month.clicks) + 1
-            }
-          })
-      }
+  } else {
+    clicksProd.push({
+      an: currentYear,
+      months: standardYearMonths
+    });
+  }
+  clicksProd.map((an) => {
+
+    if (Number(an.an) === Number(currentYear)) {
+      an.months.map((month) => {
+        if (month.month === monthNames[d.getMonth()]) {
+          month.clicks = Number(month.clicks) + 1
+        }
       })
+    }
+  })
   res.send(clicksProd);
   products_stats = clicksProd;
   fs.writeFileSync(path.join(__dirname, '/dbs/products_clicks.json'), JSON.stringify(clicksProd, getCircularReplacer()));
 });
+
+app.get("/accessApp", (req, res) => {
+  const d = new Date();
+  let currentYear = d.getFullYear();
+  const products_clicks = fs.readFileSync(path.join(__dirname, '/dbs/access_stats.json'), 'utf8');
+  let accessStats = JSON.parse(products_clicks);
+  let v = [];
+
+  if (accessStats.length > 0) {
+    let ises = false;
+    accessStats.map((an) => {
+      if (an.an === currentYear) {
+        ises = true;
+      }
+    });
+    if (!ises) {
+      accessStats.push({
+        an: currentYear,
+        months: standardYearMonths
+      });
+    }
+  } else {
+    accessStats.push({
+      an: currentYear,
+      months: standardYearMonths
+    });
+  }
+  accessStats.map((an) => {
+
+    if (Number(an.an) === Number(currentYear)) {
+      an.months.map((month) => {
+        if (month.month === monthNames[d.getMonth()]) {
+          month.clicks = Number(month.clicks) + 1
+        }
+      })
+    }
+  })
+  res.send(accessStats);
+  access_stats = accessStats;
+  fs.writeFileSync(path.join(__dirname, '/dbs/access_stats.json'), JSON.stringify(accessStats, getCircularReplacer()));
+});
 // ------------------------------------------------------------------------------------------STATS (API)-----------------------------------------------------------------------------
 
 // Accept all routes from ReactJS
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/../client/public/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../client/public/index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
